a8a211f5a9e1395d8cc1e4d9a89a494cf8d1e1c3
Danial Goodwin <danialgoodwin@gmail.com>
2020-03-28 15:53:30 -0600

    Deploy

---
+++ docs/client/client.7849bd1f.js
@@ -0,0 +1,1 @@
+import"./client.510a3652.js";
--- src/routes/products/index.svelte
+++ src/routes/products/index.svelte
@@ -6,17 +6,20 @@
 
 <h1>Interesting Products</h1>
 
-<small>Note: I'm testing Amazon affiliate links here. These are random products that I found interesting.</small>
+<small>Note: I'm testing Amazon affiliate links here. These are random products that I found interesting. Some I have purchased and some I haven't, which I will always make clear in the description.</small>
 
 <article>
   <h2>Google Home Mini speaker mount accessory</h2>
-  <p>I already purchased 2 Google Home Mini accessories that hold the device against the wall by an outlet, and I really like them.
-  Today, I randomly saw another cool holder for the Mini device. It looks like a plant! It's currently $20.99 and I'm thinking of getting on for my girlfriend.</p>
+  <p>I already purchased 2 Google Home Mini accessories that <a href="https://amzn.to/3avpSNi">hold the device against the wall by an outlet</a> with no cables showing, and I really like them.
+  Today, I randomly saw another cool holder for the Mini device. It looks like a plant! It's currently $20.99 and I'm thinking of getting one for my girlfriend. It appears to help hide the cables too.</p>
   <a target="_blank" href="https://www.amazon.com/gp/product/B07KM8FJWB/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07KM8FJWB&linkCode=as2&tag=danialgoodwin-20&linkId=1e58202829fd0cf830a2450ba52938b6">
     <img border="0" alt="Google Home Mini speaker mount accessory on Amazon that looks like a flower" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=B07KM8FJWB&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=danialgoodwin-20" >
   </a>
   <img src="//ir-na.amazon-adsystem.com/e/ir?t=danialgoodwin-20&l=am2&o=1&a=B07KM8FJWB" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 </article>
+
+<br>
+<br>
 
 <article>
   <h2>BIG TALK questions card game</h2>
